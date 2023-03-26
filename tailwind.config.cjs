3b3c2d01b0a8b8327fa565ad2a0a045415d46665
Lalo <86909609+la-lo-go@users.noreply.github.com>
2023-03-26 13:01:58 +0200

    hello there, git!

---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"
+  ],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      black: "#000",
+      white: "#fff",
+      purple: {
+        50: "#f5f3ff",
+        100: "#ede9fe",
+        200: "#ddd6fc",
+        300: "#c4b5fa",
+        400: "#a78bfa",
+        500: "#8b5cf6",
+        600: "#7c3aed",
+        700: "#6d28d9",
+        800: "#5b21b6",
+        900: "#4c1d95",
+      },
+    },
+  },
+  plugins: [],
+  plugins: [require("daisyui")],
+};
