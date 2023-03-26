2b506119f56158e2fd656e8128e34959a81c6cab
Lalo <86909609+la-lo-go@users.noreply.github.com>
2023-03-26 20:42:01 +0200

    [NEW] Search bar not show in index [ADD] svelte

---
+++ src/components/NavBarElements.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let title;
+</script>
+
+{#if title != "Home"}
+  <li class="menu-item mr-5 form-control">
+    <input type="text" placeholder="Buscar" class="input input-bordered" />
+  </li>
+{/if}
+<li class="menu-item">
+  <a href="./About">Sobre nosotros</a>
+</li>
+<li class="menu-item">
+  <a href="./Contact">Contacto</a>
+</li>
